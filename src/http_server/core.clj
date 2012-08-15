e219dd34b162fc12f902106784e765e6f4b3fe52
Adam Gooch <adamgooch@acm.org>
2012-08-15 13:52:52 -0400

    Removed swp files and updated gitignore file.  Server is hacked together to pass two tests.

--- src/http_server/core.clj
+++ src/http_server/core.clj
@@ -1,6 +1,45 @@
-(ns http-server.core)
+(ns http-server.core
+  (:use [clojure.contrib.server-socket])
+  (:import (java.io BufferedReader InputStreamReader PrintWriter FileReader)))
+
+(def port 1024)
+
+(defn process-file [out file-name]
+  (try
+    (with-open [reader (BufferedReader. (FileReader. file-name))]
+      (doseq [line (line-seq reader)] (. out (println line))))
+  (catch java.io.FileNotFoundException exception 
+    (. out (println "HTTP/1.0 404 Not Found\n\nNot Found")))))
+
+(defn header1 [status]
+  (if (= status :ok)
+    (str "HTTP/1.1 200 OK\n")))
+
+(defn header2 []
+  (str "Date: " (. (new java.util.Date) toString) "\n"))
+
+(defn header3 [file-type]
+  (if (= file-type :text)
+    (str "Content-Type: text/html\n")))
+
+(defn handle-client [in out]
+  (println "connected")
+  (def input (BufferedReader. (InputStreamReader. in)))
+  (def output (new PrintWriter out))
+  (def request (clojure.string/split (. input readLine) #" "))
+  (if (= (first request) "GET")
+    (if (= (nth request 1) "/")
+      (. output println (str (header1 :ok) (header2) (header3 :text) "\nHello World"))
+      (process-file output (nth request 1)))
+    (. output (println "Not a GET request")))
+  (. output close))
+
+
+;(defn start-server []
+;  (doto (new java.net.ServerSocket port) (.accept)))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (println "Hello, World!"))
+  (println "server started...")
+  (def server (create-server port handle-client)))
--- test/http_server/core_test.clj
+++ test/http_server/core_test.clj
@@ -1,11 +1,41 @@
 (ns http-server.core-test
   (:use clojure.test
-        http-server.core))
+        http-server.core)
+  (:import (java.io BufferedReader InputStreamReader PrintWriter)))
+
+(def header_1 "Host: 127.0.0.1:1024")
+(def header_2 "User-Agent: HTTPTool/1.1\n\n")
+
+(defn connect []
+  (new java.net.Socket '"127.0.0.1" '1024))
+
+(defn get-reader [socket]
+  (new BufferedReader (new InputStreamReader (. socket getInputStream))))
+
+(defn get-writer [socket]
+  (new PrintWriter (. socket getOutputStream)))
+
+(defn send-request [socket, message]
+  (def out (get-writer socket))
+  (def in (get-reader socket))
+  (. out println message)
+  (. out flush)
+  {:header1 (. in readLine)
+   :header2 (. in readLine)
+   :header3 (. in readLine)
+   :blank (. in readLine)
+   :message (. in readLine)}
+)
 
 (deftest http-server
-  (testing "should receive a connection on port 80")
-    (is (= 0 1)))
-;  (testing "should serve Hello World response to a request of / root")
+  (-main)
+  (testing "should accept a connection on port 1024")
+    (is (= "Hello World"
+           (:message (send-request (connect) (str "GET / HTTP/1.1\n" header_1 header_2)))))
+  (testing "should serve Hello World response to a request of / root")
+    (is (= "Hello World"
+           (:message (send-request (connect) "GET / HTTP/1.1"))))
+)
 ;  (testing "should be able to point it to a directory")
 ;  (testing "should be able to serve file from a given directory")
 ;  (testing "should respond with an appropriate error code if a given file does not exist")
